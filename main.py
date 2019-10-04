d2a78586a789727ead917c4b4b03c318379536c5
rossrco <rossrco@gmail.com>
2019-10-04 15:04:59 +0200

    redeploy project and make minor fixes

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def hello():
     data = utils.convert_feed_to_frame(feed)
 
     try:
-        latest_ad_date = bq_client.query(latest_ad_date_query).result()
+        latest_ad_date = bq_client.query(latest_ad_date_query).result().to_dataframe().iloc[0, 0]
         print('The latest ads are from {}.'.format(
               latest_ad_date.strftime('%Y-%m-%d')))
     except google_exceptions.NotFound:
@@ -41,7 +41,7 @@ def hello():
     if latest_ad_date and latest_ad_date < data.published.max():
         data = data[data.published > latest_ad_date].copy()
 
-    data.to_gbq(destination_table='stack_overflow_jobs.job_ads',
+    data.to_gbq(destination_table='stack_overflow.ads',
                 project_id=config['project_id'],
                 if_exists='append')
 
